c33b4b0a8867040f6db5f78d9454d6195a85dc8a
Shaikat Haque <shaikathaque4@gmail.com>
2024-09-16 00:00:54 +0600

    add athlete progress table

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -1,5 +1,25 @@
 insert into athletes
-  (first_name, last_name, dob, branch, email, phone)
+  (id, first_name, last_name, dob, branch, email, phone)
 values
-  ('Christiano', 'Ronaldo', '1995-04-08', 'BASHUNDHARA_SG', 'example1@gmail.com', '1234567890'),
-  ('Lionel', 'Messi', '1995-03-10', 'UTTARA_IHSB', 'example2@gmail.io', '0987654321'),
+  (1, 'Christiano', 'Ronaldo', '1995-04-08', 'BASHUNDHARA_SG', 'example1@gmail.com', '1234567890'),
+  (2, 'Lionel', 'Messi', '1995-03-10', 'UTTARA_IHSB', 'example2@gmail.io', '0987654321'),
+  (3, 'Neymar', 'Santos', '2000-01-01', 'BASHUNDHARA_SG', 'example3@gmail.io', '1029384756');
+
+
+insert into athlete_progress
+  (athlete_id, year, quarter, passing, dribbling, shooting, discipline, awareness, attendance, comments)
+values
+  -- Christiano Ronaldo
+  (1, 2024, 1, 1, 10, 10, 5, 5, 5, 'Works hard, needs to listen to coach more.'),
+  (1, 2024, 2, 2, 10, 10, 6, 6, 6, 'Improving'),
+  (1, 2024, 3, 4, 10, 10, 6, 6, 6, 'Can do better'),
+
+  -- Messi
+  (2, 2024, 1, 10, 10, 10, 10, 10, 10, 'Megesterial'),
+  (2, 2024, 2, 10, 10, 10, 10, 10, 10, 'Keep up the good work'),
+  (2, 2024, 3, 10, 10, 10, 10, 10, 10, 'Well done!'),
+
+  -- Neymar
+  (3, 2024, 1, 7, 10, 8, 7, 8, 8, 'Needs to keep working harder'),
+  (3, 2024, 2, NULL, NULL, NULL, NULL, NULL, NULL, 'Out due to injury'),
+  (3, 2024, 3, NULL, NULL, NULL, NULL, NULL, NULL, 'Still recovering from surgery');
