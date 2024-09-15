d68a853d6623d4823ec6750e8abd38531de7e9ae
Shaikat Haque <shaikathaque4@gmail.com>
2024-09-15 23:04:40 +0600

    add athletes table

---
+++ backend/supabase/seed.sql
@@ -0,0 +1,5 @@
+insert into athletes
+  (first_name, last_name, dob, branch, email, phone)
+values
+  ('Christiano', 'Ronaldo', '1995-04-08', 'BASHUNDHARA_SG', 'example1@gmail.com', '1234567890'),
+  ('Lionel', 'Messi', '1995-03-10', 'UTTARA_IHSB', 'example2@gmail.io', '0987654321'),
