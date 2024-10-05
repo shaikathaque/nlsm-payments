c9705917bd1c14fe4d1c36ae64cd8f1e368af2eb
Shaikat Haque <shaikathaque4@gmail.com>
2024-10-05 20:06:22 +0600

    complete report UI

--- frontend/emails/report.tsx
+++ frontend/emails/report.tsx
@@ -15,20 +15,43 @@ import {
 import { Tailwind, Button } from "@react-email/components";
 import * as React from "react";
 
+interface Score {
+  category: string;
+  score: number;
+};
+
 type Props = {
-  email: string;
-  amount: string;
-  branch: string;
-  athlete_name: string;
-  method: string;
-  bkash_transaction_id: string;
-  orderId: string;
   date: string;
+  first_name: string;
+  last_name: string;
+  scores: Score[]
+  comment: string;
 }
 
+const testScores: Score[] = [
+  {
+    category: "Passing",
+    score: 10
+  },
+  {
+    category: "Dribbling",
+    score: 8
+  },
+  {
+    category: "Attendance",
+    score: 5
+  },
+  {
+    category: "Discipline",
+    score: 10
+  },
+]
+
 export const NLSMReceiptEmail = ({
-  date = "",
-  athlete_name = "",
+  date = "Oct 5, 2024",
+  first_name = "Shaikat",
+  last_name = "Haque",
+  scores = testScores
 }: Props) => (
   <Tailwind
     config={{
@@ -52,6 +75,7 @@ export const NLSMReceiptEmail = ({
       </Head>
       <Preview>NL Academy Athlete Progress Report</Preview>
 
+      {/*  Header */}
       <Section>
         <Row>
           <Column>
@@ -61,95 +85,44 @@ export const NLSMReceiptEmail = ({
               alt="NLSM Logo"
             />
           </Column>
-          {/* <Column align="right">
-            <Text className="text-md">Athlete Progress Report</Text>
-          </Column> */}
         </Row>
       </Section>
 
       <Hr />
 
+      {/* Title */}
       <Section>
-            <Text className="text-center">Player Evaluation - October 2024</Text>
+            <Text className="text-center font-semibold">Player Evaluation: <span className="font-normal">Shaikat Haque</span></Text>
+            <Text className="text-center font-semibold">Date: <span className="font-normal">{date}</span></Text>
       </Section>
 
-      <Section>
-            <Text className="text-center">Shaikat Haque</Text>
+      {/* Scores */}
+      <Section className="px-5">
+        <Row>
+          <Column align="center" className="h-[40px] w-1/3">
+            <Text className="font-semibold">Category</Text>
+          </Column>
+          <Column align="center" className="h-[40px] w-1/3">
+            <Text className="font-semibold">Score <span className="font-normal">(out of 10)</span></Text>
+          </Column>
+        </Row>
+        {scores.map((score, i) => (
+          <Row key={i}>
+            <Column align="center" className="h-[40px] w-1/3">
+              <Text>{score.category}</Text>
+            </Column>
+            <Column align="center" className="h-[40px] w-1/3">
+              <Text>{score.score}</Text>
+            </Column>
+          </Row>
+        ))}
       </Section>
 
-      <Section>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Technical Ability</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Passing</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>4</Text>
-          </Column>
-        </Row>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Dribbling</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>3</Text>
-          </Column>
-        </Row>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Shooting</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>5</Text>
-          </Column>
-        </Row>
+      {/* Comments */}
+      <Section className="mt-10 px-32">
+        <Text className="font-semibold">Coaches Comments:</Text>
+        <Text className="">{lorenIpsum}</Text>
       </Section>
-
-      <Section>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Technical Ability</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Passing</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>4</Text>
-          </Column>
-        </Row>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Dribbling</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>3</Text>
-          </Column>
-        </Row>
-        <Row>
-          <Column align="center" className="h-[40px] w-1/3">
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>Shooting</Text>
-          </Column>
-          <Column align="center" className="h-[40px] w-1/3">
-            <Text>5</Text>
-          </Column>
-        </Row>
-      </Section>
-
-
-      
-
-
-      
 
 
 
@@ -159,4 +132,4 @@ export const NLSMReceiptEmail = ({
 
 export default NLSMReceiptEmail;
 
-
+const lorenIpsum = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
